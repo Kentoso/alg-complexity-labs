5a2f68a1dcac484d09a50996491230b2e4c16773
Demian Samus <demiansamus2@gmail.com>
2024-10-27 11:35:19 +0200

    Lab 3: added table method

--- lab3/crc.py
+++ lab3/crc.py
@@ -2,15 +2,17 @@ class CRC:
     def __init__(self, polynomial):
         self.polynomial = [int(bit) for bit in polynomial]
         self.poly_len = len(self.polynomial)
+        self.poly_deg = self.poly_len - 1
+        self.crc_table = self._create_crc_table()
 
     def simple(self, message):
         message = [int(bit) for bit in message]
 
-        message.extend([0] * (self.poly_len - 1))
+        message.extend([0] * self.poly_deg)
 
         register = message[: self.poly_len]
 
-        for i in range(len(message) - (self.poly_len - 1)):
+        for i in range(len(message) - self.poly_deg):
             if register[0] == 1:
                 register = [
                     reg_bit ^ poly_bit
@@ -22,5 +24,44 @@ class CRC:
             else:
                 register = register[1:] + [0]
 
-        crc_value = "".join(str(bit) for bit in register)
-        return crc_value[:-1]
+        crc_value = "".join(str(bit) for bit in register[: self.poly_deg])
+        return crc_value
+
+    def _create_crc_table(self):
+        table = []
+        polynomial_int = int("".join(map(str, self.polynomial)), 2)
+        for byte in range(256):
+            register = byte << (self.poly_deg - 8)
+            for _ in range(8):
+                if (register & (1 << (self.poly_deg - 1))) != 0:
+                    register = (register << 1) ^ polynomial_int
+                else:
+                    register <<= 1
+                register &= (1 << (self.poly_deg + 1)) - 1
+            table.append(register & ((1 << self.poly_deg) - 1))
+        return table
+
+    def _bits_to_bytes(self, bits):
+        padding_length = (8 - len(bits) % 8) % 8
+        bits_padded = [0] * padding_length + bits
+
+        bytes_list = []
+        for i in range(0, len(bits_padded), 8):
+            byte = 0
+            for bit in bits_padded[i : i + 8]:
+                byte = (byte << 1) | bit
+            bytes_list.append(byte)
+        return bytes_list
+
+    def table(self, message):
+        message_bits = [int(bit) for bit in message]
+        message_bytes = self._bits_to_bytes(message_bits)
+
+        crc = 0
+
+        for byte in message_bytes:
+            index = ((crc >> (self.poly_deg - 8)) ^ byte) & 0xFF
+            crc = ((crc << 8) ^ self.crc_table[index]) & ((1 << self.poly_deg) - 1)
+
+        crc_value = format(crc, f"0{self.poly_deg}b")
+        return crc_value
--- lab3/test_crc.py
+++ lab3/test_crc.py
@@ -7,10 +7,11 @@ polynomial = "1100000001111"
 @pytest.mark.parametrize(
     "message, expected_crc",
     [
-        ("1101001110111", "011100111010"),  # Example message
-        ("1011101010111", "110001001001"),  # Another message example
-        ("1111111111111", "100001111011"),  # All ones
-        ("0000000000001", "100000001111"),  # All zeros
+        ("1101001110111", "011100111010"),
+        ("1011101010111", "110001001001"),
+        ("1111111111111", "100001111011"),
+        ("", "000000000000"),
+        ("1", "100000001111"),
     ],
 )
 def test_crc_simple(message, expected_crc):
@@ -20,36 +21,18 @@ def test_crc_simple(message, expected_crc):
     ), f"Failed for message {message}"
 
 
-def test_crc_empty_message():
+@pytest.mark.parametrize(
+    "message, expected_crc",
+    [
+        ("1101001110111", "011100111010"),
+        ("1011101010111", "110001001001"),
+        ("1111111111111", "100001111011"),
+        ("", "000000000000"),
+        ("1", "100000001111"),
+    ],
+)
+def test_crc_table(message, expected_crc):
     crc_calculator = CRC(polynomial)
     assert (
-        crc_calculator.simple("") == "00000000000"
-    ), "Empty message should return all-zero CRC"
-
-
-def test_crc_single_bit_message():
-    crc_calculator = CRC(polynomial)
-    assert (
-        crc_calculator.simple("1") == "100000001111"
-    ), "Single-bit message with CRC-12 polynomial should return correct CRC"
-
-
-def test_crc_different_messages():
-    crc_calculator = CRC(polynomial)
-
-    crc1 = crc_calculator.simple("101010101010")
-    crc2 = crc_calculator.simple("010101010101")
-
-    assert crc1 != crc2, "CRC values for different messages should not match"
-
-
-def test_crc_repeated_calculations():
-    crc_calculator = CRC(polynomial)
-    message = "110100111011"
-
-    crc1 = crc_calculator.simple(message)
-    crc2 = crc_calculator.simple(message)
-
-    assert (
-        crc1 == crc2
-    ), "CRC calculation should be consistent across multiple calls with the same input"
+        crc_calculator.table(message) == expected_crc
+    ), f"Failed for message {message}"
